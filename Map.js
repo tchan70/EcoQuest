67da5fddfd432b117eb516e14802bf091b4283c2
Ggirassol <gi-12@hotmail.com>
2024-03-25 10:28:20 +0000

    updated location permission on map.js

--- Map.js
+++ Map.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function Map() {
-
-// const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
 // const requestLocationPermission = async () => {
 //   try {
@@ -41,12 +40,33 @@ export default function Map() {
 //     requestLocationPermission(); 
 //   }, []);
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [hasLocationPermission, setHasLocationPermission] = useState(false);
+
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setHasLocationPermission(true);
+      setLocation(location);
+    })();
+  }, []);
+
+
     return (
           <MapView
           style={styles.map}
           provider={ PROVIDER_GOOGLE }
-          // showsUserLocation
-          // showsMyLocationButton
+          showsUserLocation={hasLocationPermission}
+          showsMyLocationButton
           />
       );
 }
