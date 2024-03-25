9adc211afb858bfae0f1e57e883059669f4d0aee
Ggirassol <gi-12@hotmail.com>
2024-03-25 12:17:18 +0000

    map.js updated. permission now is working adequately

--- Map.js
+++ Map.js
@@ -1,65 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function Map() {
 
-// const requestLocationPermission = async () => {
-//   try {
-//     const granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-    
-//     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//       setHasLocationPermission(true);
-//       console.log('Location permission already granted');
-//     } else {
-//       const requestResult = await PermissionsAndroid.request(
-//         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-//         {
-//           title: 'Location Permission Required',
-//           message: 'EcoQuest needs access to your location',
-//           buttonNegative: 'Cancel',
-//           buttonPositive: 'OK',
-//         },
-//       );
-
-//       if (requestResult === PermissionsAndroid.RESULTS.GRANTED) {
-//         setHasLocationPermission(true);
-//         console.log('Location permission granted');
-//       } else {
-//         setHasLocationPermission(false);
-//         console.log('Location permission denied');
-//       }
-//     }
-//   } catch (err) {
-//     console.warn(err);
-//   }
-// };
-
-//   useEffect(() => {
-//     requestLocationPermission(); 
-//   }, []);
-
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [hasLocationPermission, setHasLocationPermission] = useState(false);
+const [hasLocationPermission, setHasLocationPermission] = useState(false);
+// const [location, setLocation] = useState(null);
 
 
-  useEffect(() => {
+  useFocusEffect(() => {
     (async () => {
       
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        console.log('Permission to access location was denied');
         return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
+      } else {
+        console.log('Permission to access location authorized')
+        //  location = await Location.getCurrentPositionAsync({});
       setHasLocationPermission(true);
-      setLocation(location);
+      // setLocation(location);
+    }
     })();
-  }, []);
-
+  });
 
     return (
           <MapView
