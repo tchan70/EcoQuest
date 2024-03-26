b5c734f35c9b3612663eb7d551b223f16aa6c59e
Drodinger <ri3yne@hotmail.com>
2024-03-26 17:30:08 +0000

    points on logged in users UserPage now update in real time

--- src/components/user/UserDetails.js
+++ src/components/user/UserDetails.js
@@ -1,12 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { ref, onValue } from 'firebase/database';
+import { db } from '../../../firebaseConfig.js';
 import UserPhoto from "./UserPhoto";
 
 export default function UserDetails({user}) {
+    const [points, setPoints] = useState('0');
+
+    
+    useEffect(() => {
+        const userDetailsRef = ref(db, `users/${user}/points`);
+        onValue(userDetailsRef, (snapshot) => {
+                const data = snapshot.val();
+                setPoints(data);
+        })
+    }, [])
+    
+
     return (
         <View style={styles.view}>
             <Text style={styles.text}>{user}</Text>
             <UserPhoto />
-            <Text style={styles.points}>USER POINTS</Text>
+            <Text style={styles.points}>{ points }</Text>
         </View>
     );
 }
