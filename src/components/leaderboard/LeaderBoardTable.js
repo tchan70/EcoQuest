e2826b00ea961c03c564f302a0d74058d1b735db
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-03-26 16:12:51 +0000

    testing queries and ordering

--- src/components/leaderboard/LeaderBoardTable.js
+++ src/components/leaderboard/LeaderBoardTable.js
@@ -1,8 +1,27 @@
 import { StyleSheet, Text, View } from "react-native"
+import { db } from "../../../firebaseConfig";
+import { ref, query, orderByValue, onValue, orderByChild, orderByKey, limitToFirst, limitToLast } from "firebase/database";
+import { useEffect, useState } from "react";
+
 
 export default function LeaderBoardTable() {
+    const [topUsersList, setTopUserList] = useState("")
+
+    const topUsers = ref(db, 'users')
+    const queriedUsers = query(ref(db, 'users'), orderByChild('points'), limitToLast(5))
+    useEffect(()=>{
+        onValue(queriedUsers, (snapshot)=>{
+        const data = snapshot.val()
+        setTopUserList(data)
+        
+    })
+    },[])
+    
+    console.log(topUsersList);
+
     return <View style={styles.view}>
         <Text style={styles.text}>LEADERBOARD TABLE</Text>
+        <Text style={styles.text}>{JSON.stringify(topUsersList)}</Text>
     </View>
 }
 
