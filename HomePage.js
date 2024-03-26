2f24623ae693828b0d9f69a382db366d27d687d8
edthuman <edwardadirvine@aol.com>
2024-03-26 09:24:18 +0000

    add components for HomePage

--- HomePage.js
+++ HomePage.js
@@ -1,9 +1,17 @@
-import { Text, View} from "react-native"
+import { StyleSheet, Text, View} from "react-native"
+import LogLitter from "./LogLitter"
+import DailyQuest from "./DailyQuest"
 
 export default function HomePage () {
-    return (
-        <View>
-            <Text>Hello from HomePage!!</Text>
-        </View>
-    )
+    return (<View style={styles.view}>
+        <Text>Home Page!!</Text>
+        <DailyQuest style={styles.daily_quest}/>
+        <LogLitter style={styles.log_litter}/>
+    </View>)
 }
+
+const styles = StyleSheet.create({
+    view: {
+        alignItems: 'center'
+    }
+})
