aae19f09dc6c4f2079c251c3e64195ba9d0a2da9
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-03-27 11:10:41 +0000

    leaderboard styling

--- src/components/leaderboard/LeaderBoardTable.js
+++ src/components/leaderboard/LeaderBoardTable.js
@@ -1,8 +1,7 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native"
-import { db } from "../../../firebaseConfig";
-import { ref, query, orderByValue, onValue, orderByChild, orderByKey, limitToFirst, limitToLast } from "firebase/database";
-import { useEffect, useState } from "react";
-
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { db } from '../../../firebaseConfig'
+import { ref, query, orderByValue, onValue, orderByChild, orderByKey, limitToFirst, limitToLast } from 'firebase/database'
+import { useEffect, useState } from 'react'
 
 export default function LeaderBoardTable() {
     const [topUsersList, setTopUserList] = useState([])
@@ -11,7 +10,7 @@ export default function LeaderBoardTable() {
     const queriedUsers = query(ref(db, 'users'), orderByChild('points'), limitToLast(10))
     useEffect(() => {
         onValue(queriedUsers, (snapshot) => {
-            const orderedUsers = [];
+            const orderedUsers = []
             snapshot.forEach((child) => {
                 console.log(child.key)
                 console.log(child.val())
@@ -22,19 +21,24 @@ export default function LeaderBoardTable() {
         })
     }, [])
 
-    console.log(topUsersList);
+    console.log(topUsersList)
 
     return (
-        <ScrollView contentContainerStyle={styles.scrollview}>
+        <View style={styles.view}>
             <Text>LEADERBOARD TABLE</Text>
-            {topUsersList.map((item, index) => {
-                return (<View style={styles.leaderboardItem} key={item.username}>
-                    <Text style={styles.ranking}>{index + 1}</Text>
-                    <Text style={styles.text}>{item.username}</Text>
-                    <Text style={styles.text}>{item.points}</Text>
-                </View>)
+            <View style={styles.rankList} >
+                {topUsersList.map((item, index) => {
+                return (
+                    <View style={styles.leaderboardItem} key={item.username}>
+                        <Text style={styles.ranking}>{index + 1} </Text>
+                        <Text style={styles.text}>{item.username} </Text>
+                        <Text style={styles.text}>{item.points}</Text>
+                    </View>
+                )
             })}
-        </ScrollView>
+            </View>
+            
+        </View>
     )
 }
 
@@ -45,36 +49,37 @@ const styles = StyleSheet.create({
         height: 620,
         backgroundColor: 'turquoise',
         alignItems: 'center',
-        justifyContent: "center",
+        justifyContent: 'center',
         marginTop: '4%',
-        borderRadius: 10
+        borderRadius: 10,
+    },
+    rankList: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        borderStyle: "solid",
+        borderWidth: 2,
+
     },
     text: {
-        color: "black",
+        color: 'black',
         fontWeight: 'bold',
         fontSize: 20,
-        width: "20%",
-        marginBottom: 30
+        borderStyle: "dashed",
+        borderWidth: 2,
+
     },
     ranking: {
-        color: "black",
+        color: 'black',
         fontWeight: 'bold',
         fontSize: 20,
-        width: "30%",
-        marginBottom: 30,
-        alignItems: "flex-start",
-        backgroundColor: 'pink'
+        alignItems: 'flex-start',
+        backgroundColor: 'pink',
     },
-    scrollview: {
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 1,
-        height: 200
-    },
+
     leaderboardItem: {
         flex: 1,
-        flexWrap: "wrap"
-        
-    }
-
-});
+        flexWrap: 'wrap',
+        backgroundColor: 'white',
+        marginBottom: 5,
+    },
+})
