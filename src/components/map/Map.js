33d3c1cd9b1517112f3daf170d9b15147769e536
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-04-03 16:09:35 +0100

    slider styling and functionality

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -8,135 +8,173 @@ import { db } from '../../../firebaseConfig'
 import { Colors } from 'react-native/Libraries/NewAppScreen'
 import { Slider } from '@miblanchard/react-native-slider'
 
-export default function Map({hasLocationPermission,setHasLocationPermission}) {
-    const [coordinates, setCoordinates] = useState([{ latitude: 0, longitude: 0 }]);
-    const [timeSliderValue, setTimeSliderValue] = useState(2);
-    const [heatmapDisplay, setHeatmapDisplay] =useState(false)
+export default function Map({ hasLocationPermission, setHasLocationPermission }) {
+  const defaultTimeValue = 1
 
+  const [coordinates, setCoordinates] = useState([{ latitude: 0, longitude: 0 }])
+  const [timeSliderValue, setTimeSliderValue] = useState(defaultTimeValue) //set when slider is released
+  const [slidingValue, setSlidingValue] = useState(defaultTimeValue) //set as slider is being moved
+  const [heatmapDisplay, setHeatmapDisplay] = useState(false)
 
-    //useEffect(()=>{
-    //    onValue(queriedPoints, (snapshot) => {
-    //        const coordinates = []
-    //        snapshot.forEach((child)=>{
-    //            const timeBlockCoordinates = child.val()
-    //            timeBlockCoordinates.forEach((coordinate) => {
-    //                coordinates.push(coordinate);
-    //            });
-    //        })
-    //        setRawCoordinates(coordinates)
-    //        setHeatmapDisplay(true)
-    //    })
-    //}, [])
+  const timeRangeTextOptions = [
+    'hour',
+    '2 hours',
+    '3 hours',
+    '4 hours',
+    '8 hours',
+    '12 hours',
+    'day',
+    '2 days',
+    '4 days',
+    'week',
+  ]
 
-    useEffect(()=>{
-        setHeatmapDisplay(false);
-        const startHours = [1, 2, 3, 4, 8, 12, 24, 48, 96, 168];
-        const startHour = startHours[timeSliderValue - 1];
-        
-        onValue(query(ref(db, 'timestampedLocations'), orderByKey(), limitToLast(startHour)), (snapshot) => {
-            const coordinatesArray = [];
-            snapshot.forEach((timeBlock) => {
-                coordinatesArray.push(timeBlock.val());
-            });
-            setCoordinates(coordinatesArray.flat());
-        })
-        setHeatmapDisplay(true);
-    }, [timeSliderValue])
+  //useEffect(()=>{
+  //    onValue(queriedPoints, (snapshot) => {
+  //        const coordinates = []
+  //        snapshot.forEach((child)=>{
+  //            const timeBlockCoordinates = child.val()
+  //            timeBlockCoordinates.forEach((coordinate) => {
+  //                coordinates.push(coordinate);
+  //            });
+  //        })
+  //        setRawCoordinates(coordinates)
+  //        setHeatmapDisplay(true)
+  //    })
+  //}, [])
 
-    useFocusEffect(() => {
-        (async () => {
+  useEffect(() => {
+    setHeatmapDisplay(false)
+    const startHours = [1, 2, 3, 4, 8, 12, 24, 48, 96, 168]
+    const startHour = startHours[timeSliderValue - 1]
 
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                //console.log('Permission to access location was denied');
-                return;
-            } else {
-                //console.log('Permission to access location authorized')
-                setHasLocationPermission(true);
-            }
-        })
+    onValue(query(ref(db, 'timestampedLocations'), orderByKey(), limitToLast(startHour)), (snapshot) => {
+      const coordinatesArray = []
+      snapshot.forEach((timeBlock) => {
+        coordinatesArray.push(timeBlock.val())
+      })
+      setCoordinates(coordinatesArray.flat())
     })
+    setHeatmapDisplay(true)
+  }, [timeSliderValue])
 
-    const renderThumb = ()=>{
-        return ( 
-            <View style={styles.thumb}>
-            <Text style={styles.thumbText}>{timeSliderValue}</Text>
-            </View>
-        )
+  useFocusEffect(() => {
+    ; async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        //console.log('Permission to access location was denied');
+        return
+      } else {
+        //console.log('Permission to access location authorized')
+        setHasLocationPermission(true)
+      }
     }
+  })
 
+  const renderThumb = () => {
     return (
-        <View style={styles.pageContainer}>
-        <View style={styles.sliderContainer}>
-        <Slider
-        renderThumbComponent={renderThumb}
-        thumbStyle={styles.sliderThumb}
-        minimumValue={1}
-        maximumValue={10}
-        step={1} 
-        style={styles.slider} 
-        value={timeSliderValue}
-        onValueChange={value => setTimeSliderValue(value)}
-        />
-        <Text style={styles.text}>{timeSliderValue}</Text>
-        </View>
-
-        <View style={styles.mapContainer}>
-        <MapView
-        // for adding points when clicking on map
-        //onPress={handlePress}
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation={hasLocationPermission}
-        showsMyLocationButton
-        >
-        {heatmapDisplay ? <Heatmap points={coordinates} radius={20} opacity={0.5} /> : null}
-        </MapView>
-        </View>
-        </View>
+      <View style={styles.thumb}>
+        {/* <Text style={styles.thumbText}>{timeSliderValue}</Text> */}
+      </View>
     )
+  }
+
+  return (
+    <View style={styles.pageContainer}>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Text style={styles.text}>Litter picked up in the last {timeRangeTextOptions[slidingValue - 1]}</Text>
+        <Slider
+          renderThumbComponent={renderThumb}
+          thumbStyle={styles.sliderThumb}
+          minimumValue={1}
+          maximumValue={10}
+          step={1}
+          style={styles.slider}
+          value={slidingValue}
+          onSlidingComplete={(value) => setTimeSliderValue(value)}
+          onValueChange={(value) => setSlidingValue(value)}
+        />
+      </View>
+
+      <View style={styles.mapContainer}>
+        <MapView
+          // for adding points when clicking on map
+          //onPress={handlePress}
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={hasLocationPermission}
+          showsMyLocationButton
+        >
+          {heatmapDisplay ? <Heatmap points={coordinates} radius={20} opacity={0.5} /> : null}
+        </MapView>
+      </View>
+    </View >
+  )
 }
 
 const styles = StyleSheet.create({
-    pageContainer:{
-        flex: 1,
-    },
-    sliderContainer: {
-        flex: 1,
-        position: "absolute",
-        padding: 30,
-        zIndex: 3,
-        width: "100%"
-    },
-    mapContainer: {
-        flex: 1,
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject
-    },
-    text: {
-        flex: 1,
-    },
-    slider: {
-        flex: 1,
-        zIndex: 1,
-    },
-    thumb: {
-        height: 60,
-        width: 60,
-        backgroundColor: "powderblue",
-        borderStyle: "solid",
-        borderWidth: 4,
-        borderColor: "black",
-        borderRadius: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        zIndex: 2,
-    },
-    thumbText: {
-        fontSize: 30,
-        margin: "auto",
-        color: "black",
-        zIndex: 2,
-    }
+  pageContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  sliderContainer: {
+    flex: 1,
+    position: 'absolute',
+    padding: 30,
+    zIndex: 3,
+    width: '100%',
+    justifyContent: "center",
+  },
+  mapContainer: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  text: {
+    position: "absolute",
+    height: 50,
+    width: 400,
+    textAlign: "center",
+    flex: 1,
+    fontSize: 22,
+    fontWeight: "500",
+    color: 'powderblue',
+    bottom: 75,
+    padding: 10,
+    zIndex: 2,
+    margin: "auto",
+    textShadowColor: "black",
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 10,
+  },
+
+  slider: {
+    flex: 1,
+    zIndex: 1,
+  },
+  thumb: {
+    height: 45,
+    width: 45,
+    backgroundColor: 'powderblue',
+    borderStyle: 'solid',
+    borderWidth: 4,
+    borderColor: 'black',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 2,
+  },
+  thumbText: {
+    fontSize: 30,
+    margin: 'auto',
+    color: 'black',
+    zIndex: 2,
+  },
 })
